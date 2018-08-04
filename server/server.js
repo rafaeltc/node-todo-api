58207fa71a8ec12dc583170160c19b6c393ba792
José Rafael <valkyre7@hotmail.com>
2018-08-04 19:20:11 +0100

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -22,9 +22,12 @@ app.post('/todos', (req,res) => {
         (err) => {res.status(400).send(err)}
     );
 
-    console.log(req.body);
+    //console.log(req.body);
 });
 
 app.listen(3000, () => {
     console.log('started on port 3000');
 })
+
+
+module.exports = {app};
