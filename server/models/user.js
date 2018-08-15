18c5209977eff909bca709d5cb2419af96933b5f
José Rafael <valkyre7@hotmail.com>
2018-08-15 11:27:11 +0100

    hash user before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email : {
@@ -69,13 +70,30 @@ UserSchema.statics.findByToken = function(token) {
         return Promise.reject();
     }
 
-    
     return User.findOne({
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+
+        user.password;
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    }
+    else {
+        next();
+    }
+});
 
 var User = mongoose.model('users', UserSchema);
 
