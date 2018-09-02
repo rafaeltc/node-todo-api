6c68421dc7f020b681231053c2a3371cccf7af42
José Rafael <valkyre7@hotmail.com>
2018-09-02 18:09:41 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -326,3 +326,23 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/tokens', () => {
+    it('should remove auth token on logout', (done) => {
+        //delete 
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+});
