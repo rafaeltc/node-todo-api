d55ad902b7355f180b37e6866fa14279018d8f17
José Rafael <valkyre7@hotmail.com>
2018-09-02 17:31:06 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+//$pull lets you remove items from an array that meet a certain criteria
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 //model method
 UserSchema.statics.findByToken = function(token) {
     var User = this;
